97072af2f78af251c80e91d673e19ece07d20b10
NexGen Projects <frasatsecond5@gmail.com>
2026-08-30 00:29:36 +0500

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="site-footer__inner">
+        <p>© 2026 WhatsApp Groups. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
