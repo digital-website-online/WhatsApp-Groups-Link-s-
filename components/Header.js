8d8a9e5109066df8de07a47cb5856443f98bd5a9
NexGen Projects <frasatsecond5@gmail.com>
2026-08-30 00:12:30 +0500

    Add Header component with navigation links

---
+++ components/Header.js
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <a href="/">WhatsApp Groups</a>
+      </div>
+
+      <nav>
+        <a href="/">Home</a>
+        <a href="/categories">Categories</a>
+        <a href="/countries">Countries</a>
+        <a href="/add-group">Add Group</a>
+      </nav>
+    </header>
+  );
+}
