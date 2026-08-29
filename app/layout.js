e79c60b05b05439aca10a92fb3e57b5d0ea1f447
NexGen Projects <frasatsecond5@gmail.com>
2026-08-30 00:06:04 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: "WhatsApp Groups",
+  description: "Find and discover WhatsApp groups.",
+};
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
