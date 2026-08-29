76aa35c484943ada977661a8d3cc7e392e7e74f9
NexGen Projects <frasatsecond5@gmail.com>
2026-08-30 00:26:26 +0500

    Create RootLayout component with metadata for app

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "../styles/global.css";
+
+export const metadata = {
+  title: "WhatsApp Groups",
+  description: "Find and discover WhatsApp groups.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
