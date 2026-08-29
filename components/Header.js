47da14227774c92b0f5384ace19c91962da3d843
NexGen Projects <frasatsecond5@gmail.com>
2026-08-30 00:43:10 +0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,18 @@
 export default function Header() {
   return (
-    <header>
-      <a href="/">WhatsApp Groups</a>
+    <header className="site-header">
+      <div className="site-header__inner">
+        <a href="/" className="site-header__logo">
+          WhatsApp Groups
+        </a>
+
+        <nav className="site-header__nav" aria-label="Main navigation">
+          <a href="/">Home</a>
+          <a href="/categories">Categories</a>
+          <a href="/countries">Countries</a>
+          <a href="/add-group">Add Group</a>
+        </nav>
+      </div>
     </header>
   );
 }
