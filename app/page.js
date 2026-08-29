2fe2e47e8f1ca7e0d92b9d68115771c576dbd3da
NexGen Projects <frasatsecond5@gmail.com>
2026-08-30 01:28:08 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Header from "../components/Header";
 import HomeHero from "../components/HomeHero";
 import CategorySection from "../components/CategorySection";
+import GroupSection from "../components/GroupSection";
 import CountrySection from "../components/CountrySection";
 import Footer from "../components/Footer";
 
@@ -12,6 +13,7 @@ export default function HomePage() {
       <main>
         <HomeHero />
         <CategorySection />
+        <GroupSection />
         <CountrySection />
       </main>
 
