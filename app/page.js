4b6d3a664570aaaf113d5952ad19286f46532a81
NexGen Projects <frasatsecond5@gmail.com>
2026-08-30 01:17:47 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,21 @@
+import Header from "../components/Header";
+import HomeHero from "../components/HomeHero";
+import CategorySection from "../components/CategorySection";
+import CountrySection from "../components/CountrySection";
+import Footer from "../components/Footer";
+
 export default function HomePage() {
   return (
-    <main>
-      <h1>WhatsApp Groups</h1>
-      <p>Find and discover WhatsApp groups.</p>
-    </main>
+    <>
+      <Header />
+
+      <main>
+        <HomeHero />
+        <CategorySection />
+        <CountrySection />
+      </main>
+
+      <Footer />
+    </>
   );
 }
