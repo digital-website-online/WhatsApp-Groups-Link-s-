9b7048a4f74c36dc1f9ebcd4f9a3aea8f9fbd96f
NexGen Projects <frasatsecond5@gmail.com>
2026-09-02 16:16:20 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -20,6 +20,7 @@ import "../styles/faq-section.css";
 import "../styles/blog-section.css";
 import "../styles/landing-page.css";
 import "../styles/topic-landing-section.css";
+import "../styles/related-topics.css";
 
 export const metadata = {
   metadataBase: new URL("https://whats-app-groups-links.vercel.app"),
